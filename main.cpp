270396dcefa79a248e54b2a4213162ff863e99e9
bboczula <boczula.bartosz@gmail.com>
2025-11-10 05:41:13 +0100

    Setup of the Bitmap class infrastructure
    I think it is the best if we decided to manage all the memory for bitmap, and only give the use the interface to manipulate the pixels. It can be improved, but this is the groundwork.

--- main.cpp
+++ main.cpp
@@ -4,28 +4,30 @@
 
 int main(int argc, char* argv[])
 {
+	BeeMP::Bitmap bmp;
+
 	// Load a BMP file into memory
-	auto result = LoadFileToMemory("../sample.bmp", true);
+	bmp.LoadFileFromMemory("../sample.bmp", true);
 
 	// Read its data
-	ReadBmpData();
+	bmp.ReadBmpData();
 
 	// Modify this data a little bit, for example set the first pixel to red
 	// Blur it a little, make a grayscale version, change its size, etc.
-	for(int i = 0; i < output.size(); i += 3)
+	for(int i = 0; i < bmp.Size(); i += 3)
 	{
 		// Simple grayscale conversion
-		unsigned char gray = static_cast<unsigned char>(0.299 * output[i] + 0.587 * output[i + 1] + 0.114 * output[i + 2]);
-		output[i] = gray;       // R
-		output[i + 1] = gray;   // G
-		output[i + 2] = gray;   // B
+		unsigned char gray = static_cast<unsigned char>(0.299 * bmp.Get(i) + 0.587 * bmp.Get(i + 1) + 0.114 * bmp.Get(i + 2));
+		bmp.Set(i, gray);
+		bmp.Set(i + 1, gray);
+		bmp.Set(i + 2, gray);
 	}
 
 	// Encode image data to a BMP raw buffer
-	EncodeBmpData(output, 512, 512, 24);
+	bmp.EncodeBmpData();
 
 	// Store the data back to a new BMP file
-	StoreBufferToFile("output.bmp", outputBmp, true);
+	bmp.StoreBufferToFile("output.bmp");
 
 	return 0;
 }
